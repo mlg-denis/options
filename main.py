9f9f032d9cc5555e864b2f4a1d72876b77edcc6b
mlg-denis <mlg-denis@outlook.com>
2025-11-18 20:13:10 +0000

    Add live output of S_T to console

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 import numpy as np
-import websocket, json, os
+import websocket, json
+from os import getenv
+from time import sleep
+from datetime import datetime, timezone
 
-FINNHUB_API_KEY = os.getenv("FINNHUB_API_KEY")
+FINNHUB_API_KEY = getenv("FINNHUB_API_KEY")
 
-def main():
-    N = 100000 # number of simulations
-    S_0 = 100 # initial price
-    mu = 0.05 # annualised drift
-    sigma = 0.2 # annualised volatlity
-    T = 1.0 # time horizon (years)
+N = 100000 # number of simulations
+mu = 0.00414 # annualised drift (risk-free rate)
+sigma = 0.2 # annualised volatlity
+T = 1.0 # time horizon (years)
 
+def calculate_S_T(S_0: float):
     Z = np.random.normal(0,1, size=N)
     W_T = np.sqrt(T) * Z
 
-    S_T = S_0 * np.exp((mu - 0.5* sigma**2)*T + sigma*W_T)
-    print(S_T)
-    
+    return S_0 * np.exp((mu - 0.5* sigma**2)*T + sigma*W_T)
+
+def main():
+    print(FINNHUB_API_KEY)
     print_prices()
 
 def print_prices():
     def on_message(ws, message):
-        data = json.loads(message)
-        print(data)
+        msg = json.loads(message)
+        now = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
+        print(f"{now}: Data received")
+
+        if msg.get("type") == "trade" and "data" in msg:
+            trades = msg["data"]
+            if trades:
+                S_0 = trades[-1]["p"] # last available price
+                S_T = calculate_S_T(S_0)
+                now = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
+                print(f"{now}: S_0 = {S_0}, S_T = {S_T}")
 
     def on_open(ws):
         ws.send(json.dumps({"type": "subscribe", "symbol": "AAPL"}))
@@ -32,8 +44,13 @@ def print_prices():
     socket = f"wss://ws.finnhub.io?token={FINNHUB_API_KEY}"
     ws = websocket.WebSocketApp(socket, on_message=on_message, on_open=on_open, on_close=on_close)
 
-    ws.run_forever()    
-
+    while True:
+        try:
+            ws.run_forever()    
+        except Exception as e:
+            print("Error: ", e)
+            sleep(5)
+            continue
 
 if __name__ == "__main__":
     main()
